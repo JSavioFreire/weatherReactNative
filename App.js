f73b126f82299aaa68b441df44c2f94c0f08ff8e
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2023-01-06 13:16:44 -0300

    weatherReactNative

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import HomePage from './pages/home/homePage';
+
 
 export default function App() {
+  const Tab = createMaterialBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator
+        initialRouteName='Home'
+        activeColor="#e91e63"
+        barStyle={{ backgroundColor: 'tomato' }}
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: true,
+          tabBarStyle: { backgroundColor: '#5B8C2A' },
+          tabBarInactiveTintColor: "#A9A9A9",
+          tabBarInactiveBackgroundColor: '',
+          tabBarActiveTintColor: "white",
+          tabBarActiveBackgroundColor: '#5B8C29',
+        }}>
+        <Tab.Screen name="Home" component={HomePage} Options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }} />
+        <Tab.Screen name="Feed" component={HomePage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/home/homePage.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+import { styleHome } from "./StyleHomePage";
+
+const HomePage = () => {
+    return (
+        <View style={styleHome.container}>
+            <Text>Olá, </Text>
+        </View>
+    )
+}
+export default HomePage;
